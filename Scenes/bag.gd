0829a39bf85afb9a3e67749f3ae823b2d3972137
Pedro Karut Gimenez <pedrokarut@hotmail.com>
2016-07-29 09:47:23 -0300

    Add files via upload

--- Scenes/bag.gd
+++ Scenes/bag.gd
@@ -7,13 +7,13 @@ var total
 func _ready():
 	bag_anim = get_node("bag_area/bag_sprite/bag_anim")
 	get_node("bag_area").connect("body_enter",self,"_Colisao")
-	lblTotal = 	get_tree().get_root().get_node("game/lblLixoColetado/lblTotal")
+
 	
 func _Colisao( body ):
 	bag_anim.play("coletado")
-	total = int(lblTotal.get_text()) + 1
-	lblTotal.set_text(var2str(total)) #function to convert a var to string
-	
+
 func _destroy():
+	Principal.qtdbag = Principal.qtdbag - 1
+	Principal.bagsColetadas = Principal.bagsColetadas + 1
+	print(Principal.qtdbag)
 	self.queue_free()
-	print("OK")
