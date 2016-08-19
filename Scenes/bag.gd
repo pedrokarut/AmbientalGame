f64bd657492989ec284f55d25927e10e51cc0b9c
Pedro Karut Gimenez <pedrokarut@hotmail.com>
2016-08-19 15:35:03 -0300

    Fase 1 consertada

--- Scenes/bag.gd
+++ Scenes/bag.gd
@@ -12,10 +12,3 @@ func _ready():
 func _Colisao( body ):
 	get_node("SamplePlayer").play("Power_Up")
 	bag_anim.play("coletado")
-
-func _destroy():
-	Principal.pontuacao = Principal.pontuacao + 10
-	Principal.qtdbag = Principal.qtdbag - 1
-	Principal.bagsColetadas = Principal.bagsColetadas + 1
-	print(Principal.qtdbag)
-	self.queue_free()
